4e8c8bf9b733755791697272d0022fdfd3d04ed5
Daniel Velázquez <danvelcab@gmail.com>
2018-09-03 12:28:44 +0200

    . cambio de versión de angular a la 6

--- demo-angular/e2e/src/app.e2e-spec.ts
+++ demo-angular/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('demo-angular App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('demo-angular App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to demo-angular!');
   });
 });
