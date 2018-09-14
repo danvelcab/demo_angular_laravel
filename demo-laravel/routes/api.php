39f20af2661cb34d85b95da734e41c3d2e024b1f
Jesus Castillo <jesus.castillo.eslava@gmail.com>
2018-09-14 10:02:49 +0200

    Router v1

--- demo-laravel/routes/api.php
+++ demo-laravel/routes/api.php
@@ -17,10 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'project'], function(){
-    Route::get('/', 'ProjectController@list');
-    Route::get('/{id}', 'ProjectController@get');
-    Route::post('/', 'ProjectController@store');
-    Route::post('/{id}', 'ProjectController@update');
-    Route::delete('/{id}', 'ProjectController@delete');
+Route::group(['prefix' => 'v1'], function(){
+    Route::group(['prefix' => 'project'], function(){
+        Route::get('/', 'ProjectController@list');
+        Route::get('/{id}', 'ProjectController@get');
+        Route::post('/', 'ProjectController@store');
+        Route::post('/{id}', 'ProjectController@update');
+        Route::delete('/{id}', 'ProjectController@delete');
+    });
 });
